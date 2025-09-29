e2389fde57389191586f0b1b2af2985a0ab89ea0
CvetkovKonstantin <praimrprrt@gmail.com>
2025-09-29 12:29:25 +0300

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-double findt(const double Q, const double I, const double R);
+double findt(const double Q_kJ, const double I, const double R);
 int main()
 {
-	double R, I, Q_kJ, Q, t;
+	double R, I, Q_kJ;
 	printf("Введите сопротивление R (Oм): ");
 	scanf("%lf", &R);
 	printf("Введите силу тока I (A): ");
 	scanf("%lf", &I);
 	printf("Введите количество теплоты Q (кДж): ");
 	scanf("%lf", &Q_kJ);
-	printf("Время нагрева проволки: %.2f секунд\n", findt);
+	printf("Время нагрева проволки: %.2f секунд\n", findt(R, I, Q_kJ));
 	return 0;
 }
 double findt(const double Q_kJ, const double I, const double R)
