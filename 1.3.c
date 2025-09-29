7c233f4e3e92899c61189d22ebcb15cb59b089da
CvetkovKonstantin <praimrprrt@gmail.com>
2025-09-29 12:49:55 +0300

    Update 1.3.c

--- 1.3.c
+++ 1.3.c
@@ -13,7 +13,9 @@ double findt(const double Q_kJ, const double I, const double R);
 */
 int main()
 {
-	double R, I, Q_kJ;
+	double R;
+	double I;
+	doucle Q_kJ;
 	printf("Введите сопротивление R (Oм): ");
 	scanf("%lf", &R);
 	printf("Введите силу тока I (A): ");
