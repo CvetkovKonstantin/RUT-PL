45ad8ceccdf2eb23f90926c856672a43715b4067
CvetkovKonstantin <praimrprrt@gmail.com>
2025-10-03 10:39:48 +0300

    2.1.c

--- 2.1.c
+++ 2.1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 /**
 *@brief ищет среднее арифметическое кубов двух чисел
 *@param num1 первое число
@@ -7,6 +8,11 @@
 *@return возвращает рассчитанное значение
 */
 double getCA(const double num1, const double num2);
+/**
+ * @brief считывает значение,введенное с клавиатуры с проверкой ввода
+ * @return считанное значение
+ */
+double getValue();
 /**
 *@brief ищет среднее геометрическое модулей двух чисел
 *@param num1 первое число
@@ -18,19 +24,16 @@ double getCG(const double num1, const double num2);
 *@brief Точка входа в программу
 *@return возвращает 0, если программа выполнена корректно
 */
-int main()
+int main(void)
 {   
-    double num1;
-    double num2;
     printf("Введите первое число: ");
-    scanf("%lf", &num1);
+    double num1=getValue();
     printf("Введите второе число: ");
-    scanf("%lf", &num2);
+    double num2=getValue();
     printf("Среднее арифметическое равно: %.2lf\n",getCA(num1, num2));
     printf("Среднее геометрическое равно: %.2lf",getCG(num1, num2));
     return 0;
 }
-
 double getCA(const double num1, const double num2)
 {
     return (pow(num1, 3)+pow(num2, 3))/2;
@@ -38,4 +41,14 @@ double getCA(const double num1, const double num2)
 double getCG(const double num1, const double num2)
 {
     return sqrt(fabs(num1)*fabs(num2));
+}
+double getValue()
+{
+    double value = 0;
+    if (!scanf("%lf",&value))
+    {
+        printf("Error\n");
+        abort();
+    }
+    return value;
 }
