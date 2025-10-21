6bda1163c7f955499111a24e22bfcfe5de284bf0
CvetkovKonstantin <praimrprrt@gmail.com>
2025-10-21 12:56:22 +0300

    2.2.c

--- 2.2.c
+++ 2.2.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h> 
+
 /**
- * @brief вычисляет значение функции при x < 4
+ * @brief Вычисляет значение функции при x < 4
  * @param x значение переменной x
  * @param a значение константы a
  * @return результат вычисления функции
  */
 double getFirst(const double x, const double a);
+
 /**
- * @brief вычисляет значение функции при x >= 4
+ * @brief Вычисляет значение функции при x >= 4
  * @param x значение переменной x
  * @param a значение константы a
  * @return результат вычисления функции
  */
 double getSecond(const double x, const double a);
+
 /**
- * @brief вычисляет значение, введенное с клавиатуры с проверкой ввода
- * @return вычисленное значение
+ * @brief Ввод значения с клавиатуры с проверкой корректности
+ * @return введённое значение
  */
-double getValue();
+double getValue(void);
+
 /**
  * @brief Точка входа в программу
  * @return возвращает 0, если программа выполнена корректно
  */
 int main(void)
 {
-    double x = 0, a = 0;
+    double x = 0.0, a = 0.0;
+
     printf("Введите значение x: ");
     x = getValue();
+
     printf("Введите значение константы a: ");
     a = getValue();
+
     printf("x = %.2lf\n", x);
     printf("a = %.2lf\n", a);
-    if (x < 4)
+
+    if (x < 4.0)
     {
-        if (x == 0)
+        if (fabs(x) < DBL_EPSILON)
         {
             printf("Ошибка: деление на ноль невозможно!\n");
         }
@@ -50,13 +59,13 @@ int main(void)
         double result = getSecond(x, a);
         printf("Результат: %.2lf\n", result);
     }
-    
+
     return 0;
 }
 
 double getFirst(const double x, const double a)
 {
-    return a/x + pow(x, 2);
+    return a / x + pow(x, 2);
 }
 
 double getSecond(const double x, const double a)
@@ -64,12 +73,12 @@ double getSecond(const double x, const double a)
     return a * x + pow(x, 3);
 }
 
-double getValue()
+double getValue(void)
 {
-    double value = 0;
-    if (!scanf("%lf",&value))
+    double value = 0.0;
+    if (scanf("%lf", &value) != 1)
     {
-        printf("Error\n");
+        printf("Ошибка ввода!\n");
         abort();
     }
     return value;
