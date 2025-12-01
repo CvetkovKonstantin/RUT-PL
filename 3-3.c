a7c2b311df8c53bd4a9b29f9b67607630717501b
CvetkovKonstantin <praimrprrt@gmail.com>
2025-12-01 20:31:22 +0300

    3-3.c

--- 3-3.c
+++ 3-3.c
@@ -1,143 +1,119 @@
-#include <stdio.h>
-#include <math.h>
-#include <float.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include<stdio.h>
+#include<math.h>  
+#include<stdlib.h>
+#include<float.h>
 
 /**
- * @brief Проверяет корректность ввода размера массива и возвращает его.
- * @param message Сообщение для пользователя.
- * @return Введенное целое число.
- */
-float validate_input(const char* message);
-
-/**
- * @brief Вычисляет значение atan(x).
- * @param x - Аргумент функции.
- * @return Значение atan(x).
- */
-double GetEPowerX(double x);
-
-/**
- * @brief Вычисляет текущий член рекуррентного ряда.
- * @param x - Аргумент ряда.
- * @param n - Порядковый номер члена ряда.
- * @return Значение текущего члена рекуррентного ряда.
- */
-double GetRecurrent(double x, double n);
-
-/**
- * @brief Вычисляет сумму ряда с заданной точностью e.
- * @param x - Аргумент ряда.
- * @param e - Точность вычисления суммы ряда.
- * @return Значение суммы ряда.
- */
-double GetSumOfSeries(double x, double e);
-
-/**
- * @brief Форматирует вывод для заданных значений аргумента, функции и ряда.
- * @param argument - Значение аргумента.
- * @param function - Значение функции (atan(x)).
- * @param series - Значение суммы ряда.
- */
-void FormatOutput(double argument, double function, double series);
-
-/**
- * @brief Проверяет корректность интервала и шага.
- * @param xStart Начальное значение интервала.
- * @param xFinish Конечное значение интервала.
- * @param xStep Шаг интервала.
- */
-void check_interval(double xStart, double xFinish, double xStep);
-
-/**
- * @brief Проверяет условия для значения epsilon.
- * @param e - Значение точности для проверки.
- * @param firstTerm - Значение первого члена последовательности.
- * @return true, если условия выполняются, иначе false.
+* @brief Рассчитывает сумму n членов ряда arctg(x)
+* @param n - число членов последовательности
+* @param x - аргумент функции
+* @return Рассчитанное значение
 */
-void checkEpsilonConditions(float e, float firstTerm);
+double defSumm(const int n, const double x);
 
 /**
- * @brief Основная функция программы.
- * @return Возвращает 0 в случае успешного выполнения программы, иначе 1.
+* @brief Рассчитывает сумму ряда arctg(x) с заданной точностью e
+* @param e - точность рассчёта
+* @param x - аргумент функции
+* @return Рассчитанное значение
+*/
+double defSummE(const double e, const double x);
+
+/**
+* @brief Считывает значение, введённое с клавиатуры, с проверкой ввода
+* @return Считанное значение
+*/
+double defValid();
+
+/**
+ * @brief Рассчитывает коэффициент рекуррентного выражения
+ * @param i - текущий индекс
+ * @param x - аргумент функции
+ * @return Рассчитанное значение коэффициента
  */
-int main() {
+double getRecurent(const int i, const double x);
 
-    const float xStart = validate_input("Введите начальное значение x: ");
-    const float xFinish = validate_input("Введите конечное значение x: ");
-    const float xStep = validate_input("Введите шаг x: ");
-    const float e = validate_input("Введите точность epsilon: ");
+/**
+* @brief Проверяет переменную на условие
+* @param input - значение проверяемой переменной
+*/
+void CheckValue(const double input);
 
-    check_interval(xStart, xFinish, xStep);
-    checkEpsilonConditions(e, xStart);
-
-    double x = xStart;
-    while (x <= xFinish + DBL_EPSILON)
-    {
-        FormatOutput(x, GetEPowerX(x), GetSumOfSeries(x, e));
-        x += xStep;
-    }
-
-    return 0;
-}
-
-float validate_input(const char* message) {
-    float input;
-    printf("%s", message);
-
-    if (scanf_s("%f", &input) != 1) {
-        printf("Ошибка ввода\a");
-        exit(EXIT_SUCCESS);
-    }
-
-    return input;
-}
-
-double GetEPowerX(const double x)
+/**
+* @brief Точка входа в программу
+* @return Возвращает 0, если программа была выполнена корректно, иначе 1
+*/
+int main(void)
 {
-    return atan(x);
+	system("chcp 1251");
+
+	printf("Введите значение x: ");
+	double x = defValid();
+
+	printf("Введите целое число n: ");
+	int n = (int) defValid();
+	CheckValue(n);
+
+	printf("Сумма первых %d членов ряда arctg(%.2f) = %.10lf\n", n, x, defSumm(n, x));
+	
+	printf("\nВведите точность e: ");
+	double e = defValid();
+	CheckValue(e);
+
+	printf("Сумма ряда arctg(%.2f) с точностью %.2e = %.10lf\n", x, e, defSummE(e, x));
+	
+	printf("\nПроверка: arctg(%.2f) = %.10lf\n", x, atan(x));
+
+	return 0;
 }
 
-double GetRecurrent(double x, double n)
+double defValid()
 {
-    return -(n * 2 + 1) * pow(x, 2) / (n * 2 + 3);
+	double valid = 0;
+	if (!scanf_s("%lf", &valid))
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	return valid;
 }
 
-double GetSumOfSeries(const double x, const double e)
+void CheckValue(const double input)
 {
-    double current = x;
-    double sum = 0;
-    double n = 0;
-
-    while (fabs(current) > e - DBL_EPSILON) {
-        sum += current;
-        current *= GetRecurrent(x, n);
-        n++;
-    }
-
-    return sum;
+	if (!(input > 0))
+	{
+		printf("Error\n");
+		exit(1);
+	}
 }
 
-void FormatOutput(const double argument, const double function, const double series)
+double defSumm(const int n, const double x)
 {
-    printf("%10.1f | %15.6f | %15.6f\n", argument, function, series);
+	double current = x;  
+	double result = current;
+	for (int i = 1; i < n; i++)  
+	{
+		current *= getRecurent(i, x);
+		result += current;
+	}
+	return result;
 }
 
-void check_interval(double xStart, double xFinish, double xStep) {
-    if (xFinish - xStart < DBL_EPSILON) {
-        printf("Неправильно введен интервал");
-        exit(EXIT_FAILURE);
-    }
-    else if (xStep < DBL_EPSILON) {
-        printf("Неправильно введен шаг");
-        exit(EXIT_FAILURE);
-    }
+double defSummE(const double e, const double x)
+{
+	double current = x; 
+	double result = 0;
+	for (int i = 1; fabs(current) > e; i++) 
+	{
+		result += current;
+		current *= getRecurent(i, x);
+	}
+	return result;
 }
 
-void checkEpsilonConditions(float e, float firstTerm) {
-    if (e > -DBL_EPSILON || e <= fabs(firstTerm)) {
-        puts("Ошибка ввода");
-        exit(EXIT_FAILURE);
-    }
+double getRecurent(const int i, const double x)
+{
+
+	return -x * x * (2.0 * i - 1.0) / (2.0 * i + 1.0);
 }
